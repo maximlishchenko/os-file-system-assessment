7f2dd3c3a8cbac72ad5346b9e4980e7c7c15c42e
MAKSIM LISTSENKO <m.listsenko.20@abdn.ac.uk>
2022-11-24 21:43:42 +0000

    upd

--- testcases/test.c
+++ testcases/test.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-  // load("ReadingExample.disk", 0);
+  load("ReadingExample.disk", 0);
   // test();
   // get_rootblock();
   // printf(("num_free_blocks = %d\n"), num_free_blocks());
   // printf("num_free_inodes = %d\n", num_free_inodes());
-  format("test.disk", 128, 4096, 80);
+  // format("test.disk", 128, 4096, 80);
+  read_inode();
 }
